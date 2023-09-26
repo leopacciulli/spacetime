5b5f4775a9bb884864a92e1198f3dd98812bbde7
Leonardo <paculli@hotmail.com>
2023-09-26 09:16:39 -0300

    Integrate with auth/login

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,16 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito por{' '}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        className="underline hover:text-gray-100"
+        href="https://www.linkedin.com/in/leonardo-pacciulli/"
+      >
+        Leonardo Pacciulli
+      </a>
+      {' '} - [LinkedIn]
+    </div>
+  )
+}
