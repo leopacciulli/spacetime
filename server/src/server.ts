88a6354f56d25a3508536d25366870cfa5b30dbe
Leonardo <paculli@hotmail.com>
2023-09-26 08:59:57 -0300

    Create auth route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,16 @@
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
 
 app.register(cors, {
   origin: true,
+})
+
+app.register(jwt, {
+  secret: 'spacetime',
 })
 
 app.register(memoriesRoutes)
